ffa10f244c5cb9daccc8691e5bf33f8357825453
Gheorghii28 <popovici.gheorghii89@gmail.com>
2023-11-19 17:22:49 +0100

    updated

---
+++ e2e/protractor.conf.js
@@ -0,0 +1,20 @@
+// protractor.conf.js
+exports.config = {
+    specs: ['./src/**/*.e2e-spec.ts'],
+    capabilities: {
+        browserName: 'chrome'
+    },
+    directConnect: true,
+    baseUrl: 'http://localhost:4200/',
+    framework: 'jasmine',
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000,
+        print: function () { }
+    },
+    onPrepare() {
+        require('ts-node').register({
+            project: require('path').join(__dirname, './tsconfig.e2e.json')
+        });
+    }
+};
